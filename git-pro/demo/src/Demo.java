7becde805afedb3a7745f9ce00f47f78fe54bab9
houbinchao <18561866097@163.com>
2019-04-11 15:37:43 +0800

    添加工程到git

---
+++ git-pro/demo/src/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("我是一个中国人");
+        System.out.println("我是一个中国人");
+        System.out.println("我是一个中国人");
+        System.out.println("我是一个中国人");
+        System.out.println("我是一个中国人");
+    }
+}
